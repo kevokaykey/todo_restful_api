66870018587f45b9c702a6870779c3991498f1e9
kevin kemboi <kkemboi08@gmail.com>
2019-10-11 16:23:27 +0300

    swagger

--- run.py
+++ run.py
@@ -16,7 +16,7 @@ SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
          SWAGGER_URL,
          API_URL,
          config={
-                  'app_name': "Seans-Python-Flask-REST-Boilerplate"
+                  'app_name': "KK swagger UI"
                }
             )
 
