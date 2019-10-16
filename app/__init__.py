9bd80b54786b71fe18d0045e360d318816a8b12d
kevin kemboi <kkemboi08@gmail.com>
2019-10-16 18:22:04 +0300

    config

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
-app.config.from_object(app_config["Production"])
+app.config.from_object("config.Production"])
 
 
 #if app.config["ENV"] == "production":
