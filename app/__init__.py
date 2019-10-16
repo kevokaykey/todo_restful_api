d5fbc71e028cee6594d130b4698d0b50657719cc
kevin kemboi <kkemboi08@gmail.com>
2019-10-16 18:19:25 +0300

    config

--- app/__init__.py
+++ app/__init__.py
@@ -6,12 +6,15 @@ from flask import Flask
 
 app = Flask(__name__)
 
-if app.config["ENV"] == "production":
-            app.config.from_object("config.Production")
-elif app.config["ENV"] == "testing":
-      app.config.from_object("config.Testing")
-else:
-      app.config.from_object("config.Development")
+app.config.from_object(app_config["Production"])
+
+
+#if app.config["ENV"] == "production":
+#            app.config.from_object("config.Production")
+#elif app.config["ENV"] == "testing":
+#      app.config.from_object("config.Testing")
+#else:
+#     app.config.from_object("config.Development")
 
           ### swagger specific ###
     
